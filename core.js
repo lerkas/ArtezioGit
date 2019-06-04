707a94117fcce1afc80b37b76bdd90fc8c2b7063
lerkas <lerkas@gmail.com>
2019-06-05 01:34:14 +0300

    core.js/deleted comments

--- core.js
+++ core.js
@@ -7,7 +7,6 @@ function fioToName(fioString) {
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
-// присмотритесь к коллекции "Set"
 function filterUnique(arr) {
 	let set = new Set(arr);
 	return [...set];
@@ -17,10 +16,8 @@ function filterUnique(arr) {
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
-// присмотритесь к методу .reduce
 function calculateSalaryDifference(arr) {
 	return Math.max(...arr)/Math.min(...arr);
-	// Я присмотрелась к .reduce, честно. Но таким способом мне нравится больше =)
 }
 
 module.exports = {
