579e64fa3cb02d1a6a778acc23a8e44f0af0ab25
lerkas <lerkas@gmail.com>
2019-06-12 23:58:43 +0300

    fixed sql file

--- pgSQL/sql_1-2.sql
+++ pgSQL/sql_1-2.sql
@@ -29,3 +29,10 @@ INSERT INTO public.employes_list(
 	
 --вывод результата
 SELECT * FROM public.employes_list
+
+--все сотрудники с зп < 45000
+SELECT * FROM public.employes_list where salary < 45000
+
+--все сотрудники, занимающие должность pre-sale manager с зп < 40000
+SELECT * FROM public.employes_list where job_role = 'pre-sale manager' and salary < 40000
+
--- pgSQL/sql_2.sql
+++
@@ -1,7 +0,0 @@
---все сотрудники с зп < 45000
-
-SELECT * FROM public.employes_list where salary < 45000
-
---все сотрудники, занимающие должность pre-sale manager с зп < 40000
-
-SELECT * FROM public.employes_list where job_role = 'pre-sale manager' and salary < 40000
