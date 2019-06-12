552c8c8c2ee2ac98fd3ae73e3995fad8d27c4349
lerkas <lerkas@gmail.com>
2019-06-12 18:46:51 +0300

    add screenshots

--- pgSQL/sql_1.sql
+++ pgSQL/sql_1.sql
@@ -1,5 +1,7 @@
+--создание БД
 CREATE DATABASE employees;
 
+--создание таблицы
 CREATE TABLE public.employes_list
 (
     id integer NOT NULL,
@@ -9,19 +11,21 @@ CREATE TABLE public.employes_list
     salary integer NOT NULL,
     PRIMARY KEY (id)
 );
-    
+
+--добавление добавление записей "оптом"
 INSERT INTO public.employes_list(
 	id, first_name, last_name, job_role, salary)
 	VALUES (1, 'Andrew', 'Kaa', 'Director of Department', 75000), 
            (2, 'Svetlana', 'Zab', 'sale manager', 50000), 
            (3, 'Antonio', 'Aver', 'pre-sale manager', 40000);
-           
+
+--вывод результата
 SELECT * FROM public.employes_list
 
+--добавление единичной записи
 INSERT INTO public.employes_list(
 	id, first_name, last_name, job_role, salary)
 	VALUES (4, 'Alexey', 'Lee', 'pre-sale manager', 35000);
 	
-           
+--вывод результата
 SELECT * FROM public.employes_list
-
